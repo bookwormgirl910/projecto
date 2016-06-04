7e98ef770647f7b52d457dee85928e2d9aeddf22
falafel72 <ksh717@student.cis.edu.hk>
2016-06-04 22:15:04 +0800

    documented code

--- js/sleeve.js
+++ js/sleeve.js
@@ -22,16 +22,16 @@ THE SOFTWARE.
 
 // settings
 
-var physics_accuracy  = 3,
+//CONSTANTS
+var physics_accuracy  = 25,
     mouse_influence   = 20,
     mouse_cut         = 0,
-    gravity           = 600,
-    cloth_height      = 40,
+    gravity           = 300,
+    cloth_height      = 70,
     cloth_width       = 10,
     start_y           = 20,
     spacing           = 7,
     tear_distance     = 150;
-
 
 window.requestAnimFrame =
     window.requestAnimationFrame ||
@@ -43,6 +43,7 @@ window.requestAnimFrame =
         window.setTimeout(callback, 1000 / 60);
 };
 
+//global variables
 var canvas,
     ctx,
     cloth,
@@ -57,43 +58,38 @@ var canvas,
         py: 0
     };
 
+/* Point class */
 var Point = function (x, y) {
 
+    //current x y
     this.x      = x;
     this.y      = y;
-    this.px     = x;
-    this.py     = y;
+
+    // x y coords from previous frames 
+    this.px     = x; 
+    this.py     = y; 
+
+    // velocity
     this.vx     = 0;
     this.vy     = 0;
+
+    //pin to these coords if any
     this.pin_x  = null;
     this.pin_y  = null;
     
+    //lines
     this.constraints = [];
 };
 
+/* updates point's location */ 
 Point.prototype.update = function (delta) {
 
-   //if (mouse.down) {
-
-        var diff_x = this.x - mouse.x,
-            diff_y = this.y - mouse.y,
-            dist = Math.sqrt(diff_x * diff_x + diff_y * diff_y);
-
-        /*if (mouse.button == 1) {
-
-            if (dist < mouse_influence) {
-                this.px = this.x - (mouse.x - mouse.px) * 1.8;
-                this.py = this.y - (mouse.y - mouse.py) * 1.8;
-            }
-
-        } else if (dist < mouse_cut) this.constraints = [];*/
-//    }
-
+    //add gravity
     this.add_force(0, gravity);
 
     delta *= delta;
-    nx = this.x + ((this.x - this.px) * .985) + ((this.vx / 2) * delta);
-    ny = this.y + ((this.y - this.py) * .985) + ((this.vy / 2) * delta);
+    nx = this.x + ((this.x - this.px) * 0.985) + ((this.vx / 2) * delta);
+    ny = this.y + ((this.y - this.py) * 0.985) + ((this.vy / 2) * delta);
 
     this.px = this.x;
     this.py = this.y;
@@ -104,6 +100,8 @@ Point.prototype.update = function (delta) {
     this.vy = this.vx = 0
 };
 
+
+/* draws all connecting lines */ 
 Point.prototype.draw = function () {
 
     if (!this.constraints.length) return;
@@ -112,21 +110,27 @@ Point.prototype.draw = function () {
     while (i--) this.constraints[i].draw();
 };
 
+/* keep points & lines within boundaries */ 
 Point.prototype.resolve_constraints = function () {
 
+    //keep all pinned points pinned
     if (this.pin_x != null && this.pin_y != null) {
         this.x = this.pin_x;
         this.y = this.pin_y;
         return;
     }
 
+    //update all line positions
     var i = this.constraints.length;
     while (i--) this.constraints[i].resolve();
 
+    //confine points + constraints to boundaries
     this.x > boundsx ? this.x = 2 * boundsx - this.x : 1 > this.x && (this.x = 2 - this.x);
     this.y < 1 ? this.y = 2 - this.y : this.y > boundsy && (this.y = 2 * boundsy - this.y);
 };
 
+
+/* add new line between two points */
 Point.prototype.attach = function (point) {
 
     this.constraints.push(
@@ -134,22 +138,26 @@ Point.prototype.attach = function (point) {
     );
 };
 
+/* remove line */
 Point.prototype.remove_constraint = function (constraint) {
 
     this.constraints.splice(this.constraints.indexOf(constraint), 1);
 };
 
+/* add external velocities to point */
 Point.prototype.add_force = function (x, y) {
 
     this.vx += x;
     this.vy += y;
 };
 
+/* set pin coordinates */
 Point.prototype.pin = function (pinx, piny) {
     this.pin_x = pinx;
     this.pin_y = piny;
 };
 
+/* keep point pinned to mouse */
 Point.prototype.stick_to_mouse = function () {
     this.x = mouse.x;
     this.y = mouse.y;
@@ -158,13 +166,16 @@ Point.prototype.stick_to_mouse = function () {
     this.pin(mouse.x,mouse.y);
 };
 
+/* Define lines between points */ 
 var Constraint = function (p1, p2) {
 
+    //takes two points and starting length
     this.p1     = p1;
     this.p2     = p2;
     this.length = spacing;
 };
 
+/* recalculate length & assign previous x y coordinates to points */
 Constraint.prototype.resolve = function () {
 
     var diff_x  = this.p1.x - this.p2.x,
@@ -172,8 +183,8 @@ Constraint.prototype.resolve = function () {
         dist    = Math.sqrt(diff_x * diff_x + diff_y * diff_y),
         diff    = (this.length - dist) / dist;
 
-    var px = diff_x * diff * 0.5;
-    var py = diff_y * diff * 0.5;
+    var px = diff_x * diff * 0.2;
+    var py = diff_y * diff * 0.2;
 
     this.p1.x += px;
     this.p1.y += py;
@@ -181,24 +192,28 @@ Constraint.prototype.resolve = function () {
     this.p2.y -= py;
 };
 
+/* redraw line */
 Constraint.prototype.draw = function () {
 
     ctx.moveTo(this.p1.x, this.p1.y);
     ctx.lineTo(this.p2.x, this.p2.y);
 };
 
+/* define cloth */
 var Cloth = function () {
 
     this.points = [];
 
     var start_x = canvas.width / 2 - cloth_width * spacing / 2;
 
+    //create grid of points
     for (var y = 0; y <= cloth_height; y++) {
 
         for (var x = 0; x <= cloth_width; x++) {
 
             var p = new Point(start_x + x * spacing, start_y + y * spacing);
 
+            //create lines between points 
             x != 0 && p.attach(this.points[this.points.length - 1]);
             //(y == 0 && x == 0) && p.pin(p.x, p.y); no longer pins to fixed point
             y != 0 && p.attach(this.points[x + (y - 1) * (cloth_width + 1)]);
@@ -208,6 +223,7 @@ var Cloth = function () {
     }
 };
 
+/* update cloth by updating lines and points */
 Cloth.prototype.update = function () {
 
     var i = physics_accuracy;
@@ -221,6 +237,7 @@ Cloth.prototype.update = function () {
     while (i--) this.points[i].update(.016);
 };
 
+/* redraw all lines & points */
 Cloth.prototype.draw = function () {
 
     ctx.beginPath();
@@ -231,6 +248,7 @@ Cloth.prototype.draw = function () {
     ctx.stroke();
 };
 
+/* update canvas */
 function update() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -241,25 +259,8 @@ function update() {
     requestAnimFrame(update);
 }
 
+/* initialise canvas */ 
 function start() {
-
-    /*
-    canvas.onmousedown = function (e) {
-        mouse.button  = e.which;
-        mouse.px      = mouse.x;
-        mouse.py      = mouse.y;
-        var rect      = canvas.getBoundingClientRect();
-        mouse.x       = e.clientX - rect.left,
-        mouse.y       = e.clientY - rect.top,
-        mouse.down    = true;
-        e.preventDefault();
-    };
-
-    
-    canvas.onmouseup = function (e) {
-        mouse.down = false;
-        e.preventDefault();
-    };*/
 
     canvas.onmousemove = function (e) {
         mouse.px  = mouse.x;
@@ -288,6 +289,7 @@ function start() {
     update();
 }
 
+/* initialise window */ 
 window.onload = function () {
 
     canvas  = document.getElementById('c');
